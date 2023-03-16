2801e19343fd7e28afd198b70affaf51e536e2fc
Amy Herbert <ahebert0020@pointloma.edu>
2023-03-16 16:37:52 -0700

    turn order

--- Assets/Scripts/TurnControl.cs
+++ Assets/Scripts/TurnControl.cs
@@ -83,66 +83,23 @@ public class TurnControl : MonoBehaviour
         {
             int turnRoll = Dice.rollD("D20");
             instructions.text = allUnits[i].tag + " rolls " + turnRoll;
-            
             DiceText.text = turnRoll.ToString();
+            string whichUnit = allUnits[i].tag + i.ToString();
+            turnDict.Add(whichUnit, turnRoll);
             yield return new WaitForSeconds(1f);
         }
-       
-        
-       //when it switches state can call the PlayerTurn() or EnemyTurn() functions and do what is needed
-
-    }
-    //probably instead of player and enemy it will be like wizard / cleric turns 
-
-    /*IEnumerator PlayerAttack()
-    {
-        //roll the dice to see results
-        int attackRoll = Dice.rollD("D20");
-        DiceText.text = attackRoll.ToString();
-        attackRoll+=3; //add 3 for attack
-        instructions.text = "Attack Roll is " + attackRoll;
-        yield return new WaitForSeconds(2f);
-
-        //either return or yield return new WaitForSeconds and then do something else
-        //can check if the enemy is dead and change to win state if necessary
-        //if skeleton
-    }
-
-    void PlayerTurn() //can make this a Corountine if needed
-    {
-        //what happens when it is a player's turn
-        DiceText.text = " "; //remove the last roll so it is blank
-        instructions.text = "Player choose move or action ";
-
-        //when done switch the state to the enemy's turn
-        //state = TurnState.enemy
-        //EnemyTurn() 
-    }
-    void EnemyTurn()
-    {
-        //do enemy stuff then call PlayerTurn again
-    }
-    public void onActionButton() //if player clicks attack button do this stuff ...
-    {
-        if(state != TurnState.player) //basically if action shouldn't be clicked
-        {
-            return; //do not do anything else
+        List<string> turnOrder = new List<string>();
+        foreach (KeyValuePair<string,int> item in turnDict.OrderBy(key => key.Value))
+        { 
+           turnOrder.Add(item.Key);
         }
-        instructions.text = "Player choose to attack or cast a spell "; //ask player to attack or cast spell
-    }
-    public void onAttackButton()
-    {
-        //do whatever attack is 
-    }
-    public void onSpellButton()
-    {
-        StartCoroutine(PlayerSpell());
-    }
-    IEnumerator PlayerSpell()
-    {
-        instructions.text = "Choose spell from spell slot ";
-        yield return new WaitForSeconds(2f);
+        turnOrder.Reverse(); //reverse the list so it is in the right order
+        for(int i = 0; i < turnOrder.Count; i++)
+        {
+            instructions.text = "order " + turnOrder[i];
+            yield return new WaitForSeconds(1f);
+        }
 
-    }*/
+    }
     
 }
