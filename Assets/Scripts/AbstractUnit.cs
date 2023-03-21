6f9f161515d78d61cc46e18c198ed1c81db46a78
colbytomita <83607990+colbytomita@users.noreply.github.com>
2023-03-21 14:21:22 -0700

    fixed bugs in AU

--- Assets/Scripts/AbstractUnit.cs
+++ Assets/Scripts/AbstractUnit.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Text;
 
-public class AbstractUnit : MonoBehavior
+public class AbstractUnit : MonoBehaviour
 {
     
     private int hp;
